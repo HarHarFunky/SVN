3a4a3cdd04d189f081605c569e2f289e2664ed1b
HarHarFunky <60793820+HarHarFunky@users.noreply.github.com>
2024-03-14 17:23:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
   <head>
-   <title>Who's logged on database</title>
+   <title>logon database</title>
   </head>
   <body>
     <form action="disconnect.php" style="position:absolute;visibility:hidden;z-index:1;<?=rand(0,1)?"left":"right"?>:<?=rand(9,92)?><?=rand(0,1)?"vw":"vmin"?>;<?=rand(0,1)?"top":"bottom"?>:<?=rand(9,92)?><?=rand(0,1)?"vh":"vmin"?>;">
